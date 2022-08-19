c5803e397751b740e0dcf591d150dfce01cec850
hyun.kim <dss99911@gmail.com>
2022-08-19 13:13:28 +0900

    update

--- awsprivatedns/js/background.js
+++ awsprivatedns/js/background.js
@@ -1,7 +1,8 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         //ex: http://ip-10-50-4-186.ap-south-1.compute.internal:8890/
-        let pattern = /(http:\/\/)ip-(\d+)-(\d+)-(\d+)-(\d+)\..+\..+\.\w+([:/].*)/i;
+        let pattern = /(http:\/\/)ip-(\d+)-(\d+)-(\d+)-(\d+)\.[\w-]+\.[\w-]+\.[\w-]+([:/].*)/i;
+
         let result = details.url.match(pattern);
         if (result) {
             return {redirectUrl: result[1] + result.slice(2, 6).join(".") + result[6]};
